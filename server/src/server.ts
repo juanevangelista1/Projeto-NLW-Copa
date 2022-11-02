a9fa3e0f78030c1b0ad52a6ddfd02ae61b074eaf
juanevangelista1 <juan.evangelista.nascimentoo@gmail.com>
2022-11-02 19:54:57 -0300

    new file:   server/.gitignore new file:   server/package-lock.json new file:   server/package.json new file:   server/prisma/dev.db new file:   server/prisma/migrations/20221102214322_create_table_pool/migration.sql new file:   server/prisma/migrations/migration_lock.toml new file:   server/prisma/schema.prisma new file:   server/src/server.ts new file:   server/tsconfig.json

---
+++ server/src/server.ts
@@ -0,0 +1,25 @@
+﻿import Fastify from 'fastify'
+import {PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient({
+  log: ['query']
+})
+
+
+async function bootstrap() {
+  const fastify = Fastify({
+    logger: true, // logger como true monitora a aplicação para ver se tem algum problema.  
+  })
+
+  fastify.get('/pools/count', async () => {
+   const count = await prisma.pool.count()
+
+    return {
+      count
+    }
+  })
+
+  await fastify.listen({port: 3333})
+}
+
+bootstrap()
